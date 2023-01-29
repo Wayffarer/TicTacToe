255dd6c66519fe6fe0e33d0898d2a047b2a5acf8
Wayffarer <hihvoin@mail.ru>
2023-01-29 15:58:46 +0300

    Window

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Запускаем игру...");
         JFrame window =new JFrame("TicTacToe");
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setSize(600,600);
+        window.setLayout(new BorderLayout());
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
         System.out.println("Конец...");
     }
 }
